9aecff4e27817e72ac8850496829eca062c4a037
Kiran <kiranpuri532@gmail.com>
2023-07-21 11:25:29 +0530

    [Kiran]Add:\ validate first name using regex

---
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Validation validation = new Validation();
+                Console.WriteLine("1- VALIDATE FIRST NAME");
+                Console.WriteLine("CHOOOSE ANY ABOVE OPTION");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        validation.Valid_FirstName();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserRegistrationProblem/Validation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class Validation
+    {
+        public void Valid_FirstName()
+        {
+            Console.WriteLine("Enter first name ");
+            string name = Console.ReadLine();
+            string firstName = "^[A-Z]{1}[a-z]{2,}$";
+            if(Regex.IsMatch(name, firstName))
+            {
+                Console.WriteLine("FIRST NAME IS VALID");
+            }
+            else
+            {
+                Console.WriteLine("INVALID FIRST NAME");
+            }
+        }
+    }
+}
